f00936fac49ea8c4f6aed6422dda519f9bfe9274
Christian Balondo <balondochristian22@gmail.com>
2021-03-18 19:03:33 +0800

    added some new buttons

--- Lumiere/Lumiere.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ Lumiere/Lumiere.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"b0227fe8-1fab-40c7-aa06-db36ce033065"
+	.asciz	"062199f7-75bc-4460-90b7-6e9a76b3e4be"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
