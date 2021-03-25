bf9ac5aa22d257c06d9f6b1528b8c52226bcdc73
Christian Balondo <balondochristian22@gmail.com>
2021-03-25 15:33:41 +0800

    fix database reset working on remaining funcitonalities

--- Lumiere/Lumiere/Pages/PageFunctions/Cashin_data.xaml.cs
+++ Lumiere/Lumiere/Pages/PageFunctions/Cashin_data.xaml.cs
@@ -27,17 +27,34 @@ namespace Lumiere.Pages.PageFunctions
             double totalBalance = double.Parse(firstTry) + double.Parse(txtAmountCashin.Text);
             try
             {
+                SQLiteConnection conn = new SQLiteConnection(App.database_location);
+                var result = conn.Query<Users>("Select * FROM Users WHERE user_id = ?", LoginPage.userEntered_ID);
+                string fullNames = "", isVerifieds = "", passwords = "", addresss = "", phonenumbers = "";
+                foreach (var x in result)
+                {
+                    fullNames = x.fullName;
+                    isVerifieds = x.isVerified;
+                    passwords = x.password;
+                    addresss = x.address;
+                    phonenumbers = x.phoneNumber;
+                }
+                conn.Close();
+                SQLiteConnection connect = new SQLiteConnection(App.database_location);
                 Users user = new Users()
                 {
                     user_id = LoginPage.userEntered_ID,
+                    fullName = fullNames,
+                    password = passwords,
+                    address = addresss,
+                    phoneNumber = phonenumbers,
                     balance = totalBalance.ToString() + ".00",
                 };
 
-                SQLiteConnection conn = new SQLiteConnection(App.database_location);
-                conn.Update(user);
-                conn.Close();
+                
+                connect.Update(user);
+                connect.Close();
                 DisplayAlert("Confirmation", "The amount " + txtAmountCashin.Text +" has been succesfully added to your account!", "okay");
-                Navigation.PushAsync(new Home());
+                
 
 
                 Transaction trans = new Transaction()
@@ -48,12 +65,12 @@ namespace Lumiere.Pages.PageFunctions
                     description = Balance.transactDesc + txtAmountCashin.Text
                 };
 
-                SQLiteConnection connect = new SQLiteConnection(App.database_location);
-                connect.CreateTable<Transaction>();
-                connect.Insert(trans);
-                connect.Close();
+                SQLiteConnection connected = new SQLiteConnection(App.database_location);
+                connected.CreateTable<Transaction>();
+                connected.Insert(trans);
+                connected.Close();
+                Navigation.PushAsync(new Home());
 
-                
             }
             catch (Exception)
             {
